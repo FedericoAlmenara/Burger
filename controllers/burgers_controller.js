e35053d923d862b4424b47e9fa9375c23c13941a
Nico Almena <fnalmenaradumur96@gmail.com>
2018-11-10 15:59:50 -0500

    setting up routes and orm still

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+var express = require("express")
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data){
+        var hndlbrsObject = {
+            burgers: data
+        };
+        console.log(hndlbrsObject);
+        res.render("index",hndlbrsObject);
+    })
+});
+
+module.exports = router;
